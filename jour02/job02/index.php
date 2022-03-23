a4240f6cdcd83a9776c4bac4d5d07516a4f7dd99
anicet-beloucif <91736488+anicet-beloucif@users.noreply.github.com>
2022-03-23 21:29:22 +0100

    Add files via upload

---
+++ jour02/job02/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+    <script src="script.js">
+    </script>
+	</head>
+<body>
+    
+    <button type="button" id="button" onclick="showhide()">Afficher le contenu</button>
+    <article id="citation">La vie a beaucoup plus d’imagination que nous</article> 
+    <div id="afficher"> <article id="citation">“L'important n'est pas la chute, mais l'atterrissage.”.</article> </div>
+</body>
+</html>
